9fcbffcde7ee4372226d0be518f8006997bd8154
icsalgado <icsalgado@icloud.com>
2021-01-21 05:53:49 -0300

    fazendo objeto pular

--- script.js
+++ script.js
@@ -10,9 +10,24 @@ function jump() {
     let position = 0;
 
     let upInterval = setInterval(() => {
-        position += 20;
-
-        dino.style.bottom = position + 'px';
+        if (position >= 150) {
+            clearInterval(upInterval);
+            //descendo
+            let downInterval = setInterval(() => {
+                if (position <=0) {
+                    clearInterval(downInterval);
+                } else {
+                    position -= 20;
+                    dino.style.bottom = position + 'px';
+                }
+                    
+            }, 20)
+        } else {
+            //subindo
+            position += 20;
+            dino.style.bottom = position + 'px';
+        }
+        
 
     }, 20);
 }

80eea38d7a901f16dbd8306256affc12fcc77265
icsalgado <icsalgado@icloud.com>
2021-01-21 06:50:27 -0300

    Adicionando formas ao dino, cactus e fundo

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const dino = document.querySelector('.dino');
 const background = document.querySelector('.background');
 let isJumping = false;
+let position = 10;
 
 function handleKeyUp(event){
     if (event.keyCode === 32) {
@@ -10,9 +11,7 @@ function handleKeyUp(event){
     }
 }
 
-function jump() {
-    let position = 10;
-
+function jump() {   
     let upInterval = setInterval(() => {
         if (position >= 150) {
             clearInterval(upInterval);
@@ -26,14 +25,12 @@ function jump() {
                     dino.style.bottom = position + 'px';
                 }
                     
-            }, 20)
+            }, 20);
         } else {
             //subindo
             position += 20;
             dino.style.bottom = position + 'px';
         }
-        
-
     }, 20);
 }
 
@@ -41,10 +38,32 @@ function jump() {
 function createCactus() {
     const cactus = document.createElement('div');
     let cactusPosition = 1000;
+    let randomTime = Math.random() * 5000;
 
     cactus.classList.add('cactus');
     cactus.style.left = 1000 + 'px';
     background.appendChild(cactus);
+
+    let leftInterval = setInterval(() => {
+        cactusPosition -= 10;
+        cactus.style.left = cactusPosition + 'px';
+
+        if (cactusPosition < -60) {
+            clearInterval(leftInterval);
+            background.removeChild(cactus);
+        } else if (cactusPosition > 0 && cactusPosition < 60 && position < 60) {
+            //game over
+
+            clearInterval(leftInterval);
+            document.body.innerHTML = '<h1 class="game-over">Fim de Jogo</h1>';
+        } else {
+            cactusPosition -= 10;
+            cactus.style.left = cactusPosition + 'px';    
+        }
+    }, 20);
+
+    setTimeout(createCactus, randomTime);
+
 }
 
 createCactus();
